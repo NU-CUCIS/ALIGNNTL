d19b9ee7deda6576138f849ded06f626ea252b3a
GuptaVishu2002 <32040675+GuptaVishu2002@users.noreply.github.com>
2022-07-06 00:07:36 -0500

    Add files via upload

---
+++ FineTuning/setup.py
@@ -0,0 +1,47 @@
+"""ALIGNN: Atomistic LIne Graph Neural Network.
+
+https://jarvis.nist.gov.
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alignn",
+    version="2021.11.16",
+    author="Kamal Choudhary, Brian DeCost",
+    author_email="kamal.choudhary@nist.gov",
+    description="alignn",
+    install_requires=[
+        "numpy>=1.19.5",
+        "scipy>=1.6.1",
+        "jarvis-tools>=2021.07.19",
+        "torch>=1.7.1",
+        "dgl-cu101>=0.6.0",
+        "dgl>=0.6.0",
+        "scikit-learn>=0.22.2",
+        "matplotlib>=3.4.1",
+        "tqdm>=4.60.0",
+        "pandas>=1.2.3",
+        "pytorch-ignite>=0.4.7",
+        "pydantic>=1.8.1",
+        "flake8>=3.9.1",
+        "pycodestyle>=2.7.0",
+        "pydocstyle>=6.0.0",
+        "pyparsing>=2.2.1,<3",
+    ],
+    # scripts=["alignn/alignn_train_folder.py"],
+    scripts=['alignn/pretrained.py','alignn/train_folder.py'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usnistgov/alignn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
