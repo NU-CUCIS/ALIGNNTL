9dbd601620dbbe3e8a5357d3a092a09499d779f2
GuptaVishu2002 <32040675+GuptaVishu2002@users.noreply.github.com>
2022-07-12 01:04:15 -0500

    Update create_features.sh

--- FeatureExtraction/create_features.sh
+++ FeatureExtraction/create_features.sh
@@ -1,3 +1,3 @@
-for filename in /data/vgf3011/alignntldata/jid/*.vasp; do
-    python alignn/pretrained_activation.py --model_name mp_e_form_alignnn --file_format poscar --file_path "$filename" --output_path "/data/vgf3011/alignntldata/data"
+for filename in ../examples/*.vasp; do
+    python alignn/pretrained_activation.py --model_name mp_e_form_alignnn --file_format poscar --file_path "$filename" --output_path "../examples/data"
 done
